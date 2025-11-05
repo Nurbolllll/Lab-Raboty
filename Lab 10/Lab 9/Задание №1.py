fa784b5ec40aa47dd77192b202438e14521e31f4
Nurbolllll <ntalgatov2007nur@gmail.com>
2025-11-05 14:43:04 +0500

    Delete Lab 10/Lab 9 directory

--- Lab 10/Lab 9/Задание №1.py
+++
@@ -1,6 +0,0 @@
-def recursive_sum(n):
-    if n == 1:
-        return 1
-    return n + recursive_sum(n - 1)
-num = 10
-print(f"Сумма чисел от 1 до {num} равна {recursive_sum(num)}")	
--- Lab 10/Lab 9/Задание №2.py
+++
@@ -1,7 +0,0 @@
-def find_max(arr):
-    if len(arr) == 1:
-        return arr[0]
-    max_rest = find_max(arr[1:])
-    return arr[0] if arr[0] > max_rest else max_rest
-numbers = [4, 15, 2, 9, 30, 7, 22]
-print("Максимальное число в списке:", find_max(numbers)) 
